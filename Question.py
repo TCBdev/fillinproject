41ae9664ced08e6afaad6f564a8616d6c226ac22
TCdev <37277083+TCdev@users.noreply.github.com>
2018-04-24 20:45:28 -0700

    Delete Question.py

--- Question.py
+++
@@ -1,5 +0,0 @@
-class Question:
-    def __init__(self, prompt, easy_answers, intermediate_answers, hard_answers, ultimate_answers):
-        self.prompt = prompt
-        self.answer = easy_answers, 
-
